62177b2b36b7d57f70b433d96341540191bca23d
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-12 18:24:59 -0400

    Create a Subscription:
    - Create a subject to push posts to list page as we create them
    - Create  getPostUpdatelistner which returns the subject as an Observable
    - Subscribe to the getPostUpdateListner from postsService in Post List Component
    - Set the fetched data to the posts in the subscribe block
    - Create a postsSub Subscription and assign it the service
    - Use ngOnDestroy life cycle hook to destroy the subscription
    - The subscription gets destroyed when we move away from that page ( for future implementation)

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 
@@ -8,17 +8,26 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   // { title: 'First Post', content: 'This is the First post\'s content' },
   // { title: 'Second Post', content: 'This is the Second post\'s content' },
   // { title: 'Third Post', content: 'This is the Third post\'s content' }
   // ];
   posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) { }
 
   ngOnInit(): void {
     this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListner()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
   }
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+
 import { Post } from './post.model';
 
 @Injectable({
@@ -6,9 +8,14 @@ import { Post } from './post.model';
 })
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts(): Post[] {
     return this.posts;
+  }
+
+  getPostUpdateListner(): Observable<Post[]> {
+    return this.postsUpdated.asObservable();
   }
 
   addPost(title: string, content: string): void {
@@ -16,5 +23,6 @@ export class PostsService {
     // title: title ----> title (can be written like this)
     const post: Post = { title, content };
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
