677c919962251672c39ded2b5fbe47d6363b47e9
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-12 09:49:06 -0400

    Use string interpolation to ouput data on button click

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  newPost = '';
   onAddPost(): void {
-    alert('Post Added!');
+    this.newPost = 'The user\'s Post';
   }
 }
