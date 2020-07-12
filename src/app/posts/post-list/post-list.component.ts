e67e66ec26828aa933fb7772ee6ee061d29db47b
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-12 17:33:28 -0400

    Use the new Posts Service:
    - Update Post List & Post Create Components to use the posts service
    - Remove the Input & Output decorators in the corresponding components

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
@@ -8,13 +8,17 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
   // posts = [
   // { title: 'First Post', content: 'This is the First post\'s content' },
   // { title: 'Second Post', content: 'This is the Second post\'s content' },
   // { title: 'Third Post', content: 'This is the Third post\'s content' }
   // ];
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
 
   constructor(public postsService: PostsService) { }
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+  }
 }
