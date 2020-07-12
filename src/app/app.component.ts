769c52651a0d670b3c77f22c9da25c156a8e6b3e
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-12 17:43:35 -0400

    Remove the 2-way bindings:
    - Update the app component & html  to remove the 2-way bindings
    - The 2-way binding is omitted as we will use RxJs library to handle it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
 
-import { Post } from './posts/post.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  StoredPosts: Post[] = [];
-
-  onPostAdded(post): void {
-    this.StoredPosts.push(post);
-  }
-}
+export class AppComponent { }
