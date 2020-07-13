67e00d761a1c3ccf52740214827e37406737f22e
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-13 00:35:24 -0400

    Add middleware to fix CORS issue & set headers

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,27 @@ const express = require('express');
 
 const app = express();
 
+//middleware to handle CORS issue
+
+app.use((req, res, next) => {
+  // which domains are allowed access to resource
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // restrict to domains sending request with a certain set of headers
+  // apart from the default headers
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  // we control which http verbs are allowed to send requests
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use('/api/posts', (req, res, next) => {
   const posts = [
     {
