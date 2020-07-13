1c9ce029305214ce8cc8625818e0bfcbae3a153d
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-13 10:47:56 -0400

    Update post action to save posts in database

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,7 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added sucessfully'
   });
