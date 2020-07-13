913743f8c54f3d5b112ad7064be5af0bf3ead9da
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-13 10:48:25 -0400

    update get endpoint to fetch the posts from database

--- backend/app.js
+++ backend/app.js
@@ -52,22 +52,13 @@ app.post('/api/posts', (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'ajd1sj213asdf',
-      title: 'First server-side post',
-      content: 'This is coming from server'
-    },
-    {
-      id: 'khad23jk56hfa',
-      title: 'Second server-side post',
-      content: 'This is coming from server!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched succesfully!',
-    posts: posts
-  });
+  Post.find()
+    .then((documents) => {
+      res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
