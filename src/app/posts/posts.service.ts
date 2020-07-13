85724581232268232a1d00d25086b3ad955aa310
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-13 11:01:02 -0400

    Transform _id to id on the Angular Side:
    - Use the pipe block to modify the data fetched from get
    - import the map operator from rxjs
    - use basic javascript map to transform the id to _id
    - Update the subscribe block to use the new transformedPosts data

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -14,9 +15,21 @@ export class PostsService {
   constructor(private http: HttpClient) { }
 
   getPosts(): void {
-    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http
+      .get<{ message: string, posts: any[] }>(
+        'http://localhost:3000/api/posts'
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
