32111715c176d9804177d64f668a1ea4bafd1d02
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-13 10:21:48 -0400

    Import Post Model to express app & update post request api to use the model to save data

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Post = require('./models/post');
+
 const app = express();
 
 //middleware to parse the json object and add it as body to response
@@ -27,9 +29,11 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: 'Post added sucessfully'
