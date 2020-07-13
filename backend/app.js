67ab92eb61280006c34c04a2a4a75c9db78dd7b1
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-13 00:04:28 -0400

    Create a sample endpoint for posts:
    - Create a sample endpoint for the path api/posts
    - Send some dummy posts data in the form of JSON

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First Middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'ajd1sj213asdf',
+      title: 'First server-side post',
+      content: 'This is coming from server'
+    },
+    {
+      id: 'khad23jk56hfa',
+      title: 'Second server-side post',
+      content: 'This is coming from server!'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
