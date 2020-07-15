f9716bc7ec16145a7708c6b3bb8457f2c6a26236
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-14 23:26:17 -0400

    Create an image preview using FileReader JS function

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit {
   enteredContent = '';
   isLoading = false;
   form: FormGroup;
+  imagePreview: string;
   private mode = 'create';
   private postId: string;
   public post: Post;
@@ -61,8 +62,11 @@ export class PostCreateComponent implements OnInit {
     const file = (event.target as HTMLInputElement).files[0];
     this.form.patchValue({ image: file });
     this.form.get('image').updateValueAndValidity();
-    console.log(file);
-    console.log(this.form);
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost(): void {
