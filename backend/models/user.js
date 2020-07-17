b840e1c66d26b16c9cbd2485bd2fd6eed1058e00
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-17 11:28:45 -0400

    Create BE api to create a new user on signup
    - Install bcrypt package for encrypting passwords to hashed passwords & add salt
    - Update User routes singup endpoint to create user & bcrypt the password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // uniqueValidator is a plugin - feature provided by moongose
 // cannot add plugins directly to schema
-const uniqueValidator = require('mongoose-unique-validator')
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
