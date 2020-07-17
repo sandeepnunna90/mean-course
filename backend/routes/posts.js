b6505f37602043e97044f7566cb92cacde41b767
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-16 22:14:27 -0400

    Add imagePath

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -27,14 +27,21 @@ const storage = multer.diskStorage({
 });
 
 router.post('', multer({ storage: storage }).single('image'), (req, res, next) => {
+  const url = req.protocol + '://' + req.get(host);
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
+    imagePath: url + '/images/' + req.file.filename
   });
   post.save().then(createdPost => {
     res.status(201).json({
       message: 'Post added sucessfully',
-      postId: createdPost._id
+      post: {
+        id: createdPost._id,
+        title: createdPost.title,
+        content: createdPost.content,
+        imagePath: createdPost.imagePath
+      }
     });
   });
 });
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -25,7 +25,8 @@ export class PostsService {
           return {
             title: post.title,
             content: post.content,
-            id: post._id
+            id: post._id,
+            imagePath: post.imagePath
           };
         });
       }))
@@ -53,15 +54,16 @@ export class PostsService {
     postData.append('content', content);
     postData.append('image', image, title);
     this.http
-      .post<{ message: string, postId: string }>(
+      .post<{ message: string, post: Post }>(
         'http://localhost:3000/api/posts',
         postData
       )
       .subscribe(responseData => {
         const post: Post = {
-          id: responseData.postId,
+          id: responseData.post.id,
           title,
-          content
+          content,
+          imagePath: responseData.post.imagePath
         };
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
@@ -71,7 +73,7 @@ export class PostsService {
 
   updatePost(id: string, title: string, content: string): void {
     // short hand notation
-    const post: Post = { id, title, content };
+    const post: Post = { id, title, content, imagePath: null };
     this.http.put('http://localhost:3000/api/posts/' + id, post)
       .subscribe(response => {
         const updatedPosts = [...this.posts];
