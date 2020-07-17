448c0dfcad2ae52013299acda590c7329efe2fb8
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-17 15:07:24 -0400

    Create a login backend API with JSON Web Token (JWT):
    - Install the jsonwebtoken node package
    - Create a POST endpoint for login
    - Authenticate the user & password
    - If the user exists, create a jwt for with the user credentials with a secret
    - Set the expiry to one hour

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const user = require('../models/user');
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
@@ -23,6 +25,38 @@ router.post('/signup', (req, res, next) => {
             error: err
           });
         });
+    });
+});
+
+router.post('/login', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'User does not exist'
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Authorization Failed'
+        });
+      }
+      const token = jwt.sign(
+        { email: user.email, userId: user._id },
+        'secret_this_should_be_longer',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        token: token
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'Authorization Failed'
+      });
     });
 });
 
