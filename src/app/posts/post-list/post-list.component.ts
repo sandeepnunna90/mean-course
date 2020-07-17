a64326f7738778d5cfbceaf4871cc246706b6b5d
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-16 23:47:40 -0400

    Connect Angular Paginator to Backend:
    - Add query params to getPosts angular service
    - Update onChangedPage & ngOnInit method to use the new page properties

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -19,6 +19,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   isLoading = false;
   totalPosts = 10;
   postsPerPage = 2;
+  currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription;
 
@@ -26,7 +27,7 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService.getPostUpdateListner()
       .subscribe((posts: Post[]) => {
         this.isLoading = false;
@@ -35,7 +36,10 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   onChangedPage(pageData: PageEvent): void {
-    console.log(pageData);
+    // +1 is needed as backend page numbers starts from 1 unlike UI which start from 0
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
 
   onDelete(postId: string): void {
