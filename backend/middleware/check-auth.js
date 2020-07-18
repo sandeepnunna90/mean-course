b54c5e6dd93edc7b9a0461355daa7faa11877644
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-18 19:18:34 -0400

    Add UserID to Posts (backend):
    - Fetch the userId & email from the decodedToken information  in checkAuth Middleware
    - Add the userData information to request in middleware
    - Access the userData from the middleware and add it to create post endpoint

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,7 +4,8 @@ module.exports = (req, res, next) => {
   // split is needed to remove the 'bearer' string from the authorization
   try {
     const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, 'secret_this_should_be_longer');
+    const decodedToken = jwt.verify(token, 'secret_this_should_be_longer');
+    req.userData = { email: decodedToken.token, userId: decodedToken.userId };
     next();
   } catch (error) {
     res.status(401).json({ message: 'Auth failed!' });
