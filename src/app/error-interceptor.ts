9a5cdbcc9aadee596e6564d4e55ca44852038903
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-07-18 22:44:18 -0400

    Create Error Intercepter:
    - This error interceptor is created to handle all the Http errors accross the app
    - Create a new interceptor file on the applicaiton folder
    - Import the interceptor in app module & include it in provider

---
+++ src/app/error-interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export class ErrorInterceptor implements HttpInterceptor {
+
+  // Observable<HttpEvent> typedef not in course but from google
+  // https://angular.io/api/common/http/HttpInterceptor#intercept
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    // handle(req) can also listen to the response
+    // handle gives us back response observable stream. we can hook into that stream
+    // and listen to the events. we can use a pipe method provided by rxjs to add
+    // an operator to that stream
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.log(error);
+        alert(error.error.error.message);
+        return throwError(error);
+      })
+    );
+  }
+}
